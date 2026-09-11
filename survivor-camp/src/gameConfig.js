6b65902097a6a4e6786b340d743ab6fe1fb8c3e8
Liv Kohring <94705557+okohring@users.noreply.github.com>
2026-09-11 12:36:20 -0500

    Add server-owned Survivor Camp game config

---
+++ survivor-camp/src/gameConfig.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = Object.freeze({
+  campPhaseSeconds: 30,
+  tugOfWar: Object.freeze({
+    durationSeconds: 12,
+    playerPullPerInput: 6.2,
+    opponentPullPerSecond: 17,
+    opponentWaveAmplitude: 2.5,
+    winThreshold: 100
+  })
+});
